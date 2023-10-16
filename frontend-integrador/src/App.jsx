4cdf9decb7727fe7d5762943d6ac6c52eeba1da7
joel <tec.joelacosta@gmail.com>
2023-10-16 15:04:00 -0300

    Estructura basica

--- frontend-integrador/src/App.jsx
+++ frontend-integrador/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/pages/home/HomePage";
+import LandingPage from "./components/pages/landing/LandingPage";
+import LoginPage from "./components/pages/login/LoginPage";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/home/:username" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
