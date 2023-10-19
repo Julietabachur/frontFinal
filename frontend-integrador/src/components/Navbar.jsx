e19008c406bf0deeaa786600763431d7a5a3df0b
GodSavitar19 <106354483+GodSavitar19@users.noreply.github.com>
2023-10-18 22:27:01 -0300

    Header, Categorias

--- frontend-integrador/src/components/Navbar.jsx
+++ frontend-integrador/src/components/Navbar.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar desarrollo</div>
-  )
-}
+    <header
+      style={{
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 20px', 
+        zIndex: 1000, 
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <a
+          href="/" 
+          style={{
+            textDecoration: 'none',
+            color: 'white',
+            marginRight: '20px', 
+          }}
+        >
+          <img
+            src="/HOME" 
+            alt="Logo"
+            style={{
+              height: '30px', 
+            }}
+          />
+        </a>
+        <span
+          style={{
+            color: 'white',
+          }}
+        >
+          Lema de la Empresa
+        </span>
+      </div>
+      <div>
+        <button
+          style={{
+            marginRight: '10px',
+          }}
+        >
+          Crear cuenta
+        </button>
+        <button>Iniciar sesión</button>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ frontend-integrador/src/components/pages/home/ProductCard.jsx
@@ -0,0 +1,26 @@
+import {
+  Box,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  HStack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+
+const ProductCard = ({ item }) => {
+  
+  return (
+    <Card h={'600px'} bg={"blackAlpha.600"} color={"whiteAlpha.700"} borderRadius={10} overflow='hidden' >
+      
+      <CardBody display={"flex"} justifyContent={"center"} >
+        <Image src={item?.thumbnail} fallbackSrc='https://via.placeholder.com/600' object-fit={"cover"} w={'100%'} ></Image>
+      </CardBody>
+{/*       <CardHeader>{item?.name}</CardHeader>
+      <CardFooter>{item?.set}</CardFooter> */}
+    </Card>
+  );
+};
+
+export default ProductCard;
