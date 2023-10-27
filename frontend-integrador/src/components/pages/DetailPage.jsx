2b7255e001382f0c7960db257c18464085ae30e1
Vanina Castelo <v.i.castelo@gmail.com>
2023-10-26 18:43:33 -0700

    modifico detalles y gallery

--- frontend-integrador/src/components/pages/DetailPage.jsx
+++ frontend-integrador/src/components/pages/DetailPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductGallery from './ProductGallery';
 
-const DetailPage = () => {
+const Detail = () => {
+  const [product, setProduct] = useState({});
+  const token = import.meta.env.VITE_TOKEN;
+  const baseUrl = import.meta.env.VITE_SERVER_URL;
+  const { id } = useParams();
+
+  const fetchProduct = async () => {
+    try {
+      const response = await fetch(`${baseUrl}/api/v1/public/products/${id}`);
+      const data = await response.json();
+      setProduct(data);
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, [id]);
+
   return (
-    <div>DetailPage</div>
-  )
-}
+    <>
+      <div className="conteiner-product">
+        {product && (
+          <div key={product.id} data={product}>
+            <figure className="container-img">
+              <img src={product.image} alt={product.title} />
+              <div>
+                <ProductGallery productImages={product.images} />
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+                <h4>{product.price}</h4>
 
-export default DetailPage
+              </div>
+            </figure>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Detail;
