25a7203a2e8b3adea6fedf1c72114bfcdbb9526f
Julieta BACHUR <jbachur@CNP.com.ar>
2023-11-06 20:56:40 -0300

    creo barra de categorias

--- frontend-integrador/src/components/pages/home/FilterBar.jsx
+++ frontend-integrador/src/components/pages/home/FilterBar.jsx
@@ -1,21 +1,23 @@
-import { Checkbox, Text, VStack, Button , HStack} from '@chakra-ui/react';
+import { Checkbox, Text, VStack, Button , HStack, Card, CardBody, Image, CardFooter} from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import ProductCardContainer from './ProductCardContainer';
 
 const FilterBar = () => {
 const token = import.meta.env.VITE_TOKEN;
 const baseUrl = import.meta.env.VITE_SERVER_URL;
 const [media, setMedia] = useState(false);
 const [categories, setCategories] = useState([]);
+const [selectedCategories, setSelectedCategories] = useState([]);
 const [filteredProductCount, setFilteredProductCount] = useState(0);
 const MIN_DESKTOP_WIDTH = 768;
 
 useEffect( () => {
-        axios.get(`${baseUrl}/api/v1/public/category/all`)
+    const urlCategories = `${baseUrl}/api/v1/public/category/all`
+        axios.get(urlCategories)
       .then((response) => {
-        debugger;
         // La respuesta exitosa se encuentra en response.data
-        setCategories(response)
+        setCategories(response.data)
         console.log('Categorías:', categories);
       })
       .catch((error) => {
@@ -25,7 +27,12 @@ useEffect( () => {
     
 ,[])
 
-
+// funcion para agregar categoria a categoriesSelected
+const addCategory=(cat)=>{
+  setSelectedCategories([...selectedCategories, cat]);
+    console.log('catSelected: ', selectedCategories);
+    countFilteredProducts()
+}
   // Función para contar la cantidad de productos filtrados
   const countFilteredProducts = () => {
     const filteredCount = categories.filter((category) => category.isChecked).length;
@@ -34,9 +41,7 @@ useEffect( () => {
 
   // Función para limpiar los filtros
   const clearFilters = () => {
-    categories.forEach((category) => {
-      category.isChecked = false;
-    });
+    setSelectedCategories([]);
     setFilteredProductCount(0);
   };
 
@@ -45,16 +50,12 @@ useEffect( () => {
     const handleResize = () => {
       if (window.innerWidth < MIN_DESKTOP_WIDTH) {
         setMedia(true);
+        console.log('media: ', media);
       } else {
         setMedia(false);
+        console.log('media: ', media);
       }
-    };
-
-    if (window.innerWidth < MIN_DESKTOP_WIDTH) {
-      setMedia(true);
-    } else {
-      setMedia(false);
-    }
+    };   
 
     window.addEventListener('resize', handleResize);
 
@@ -65,90 +66,125 @@ useEffect( () => {
   }, []);
 
   return (
-    // <VStack align="start" justifyContent="start" bg="black" w="200px" p={4} h={"100vh"}>
-    //   <Text fontSize="md" color="verde2">
-    //     Mostrando {filteredProductCount} de {categories.length} productos.
-    //   </Text>
 
-    //   {categories.map((category) => (
-    //     <Checkbox
-    //       key={category.id}
-    //       size="md"
-    //       isChecked={category.isChecked}
-    //       onChange={() => {
-    //         category.isChecked = !category.isChecked;
-    //         countFilteredProducts();
-    //       }}
-    //     >
-    //       <Text fontSize="md" color="verde2">
-    //         {category.name}
-    //       </Text>
-    //     </Checkbox>
-    //   ))}
+    <VStack>
 
-    //   <Button
-    //     size="md"
-    //     colorScheme="red" // Puedes elegir el color que desees
-    //     onClick={clearFilters}
-    //     mt={4} // Margen superior para separar el botón de los checkboxes
-    //   >
-    //     Limpiar Filtro
-    //   </Button>
-    // </VStack>
- 
-    // {!media &&
-    //     (
-        <HStack justify={"space-around"} h={35} w={"100%"} bg={"negro"}>
-
-
-        //    Muestra las tarjetas de categorías 
-          {categories.map((category) => (
-            <ProductCardContainer key={category.id}>
-                   <Card  
-                      h={['350px','300px', "300px"]}
-                      w={['300px','300px',"300px"]} 
-                      color={"blanco"}>
-                        <CardBody
+        {media ? 
+                (
+                    <>
+                    <HStack justify={"space-around"} w={"100%"} h={'100%'} my={5} display={"flex"} wrap={'wrap'} bg={"#444444"}>
+                    {/* //    Muestra las tarjetas de categorías  */}
+                    {categories.map((category) => (                    
+                        <ProductCardContainer key={category.id} onClick={addCategory(category)}>
+                            <Card  
                             _hover={{
-                            transform: "scale(1.02)", // Escala un poco la tarjeta en el hover
-                            boxShadow: "md", // Agrega una sombra al hacer hover
-                            cursor: "pointer", // Cambia el cursor al pasar por encima
-                            }}
-                            border={"5px solid black"}
-                            boxShadow={"5px 5px 5px gray"}
-                            display={"flex"}
-                            justifyContent={"center"}
-                            h={['100%']}
-                            w={['100%']}
-                            p={0}
+                                transform: "scale(1.02)", // Escala un poco la tarjeta en el hover
+                                //boxShadow: "md", // Agrega una sombra al hacer hover
+                                cursor: "pointer", // Cambia el cursor al pasar por encima
+                                }}
+                                border={"5px solid black"}
+                                h={'50px'}
+                                w={'150px'} 
+                                color={"blanco"}
+                                alignContent={"center"} justify={"center"} display={"flex"} alignItems={'center'}>                         
+                                <CardFooter color={"negro"} alignContent={"center"} justify={"center"} display={"flex"} alignItems={'center'} p={0}>
+                                    <Text
+                                    fontFamily={"Saira"}
+                                    color={"gris1"}
+                                    fontSize={'1rem'}
+                                    whiteSpace="nowrap"
+                                    overflow="hidden"
+                                    textOverflow="ellipsis"
+                                    maxWidth="100%"
+                                    >
+                                    {category.categoryName}
+                                    </Text>
+                                </CardFooter>
+                            </Card>
+                        </ProductCardContainer>
+                    )
+                    )}                   
+                    </HStack>
+                    <HStack justify={"space-around"} w={"100%"} h={'100%'} my={5} display={"flex"} wrap={'wrap'} bg={"#444444"}>
+                    {/* //    Muestra las tarjetas de categorías  */}
+                    <Text>Cantidad de productos: {categoriesSelected.le}</Text>
+                    {categoriesSelected.map((category) => (                    
+                        <Checkbox
+                        fontFamily={"Saira"}
+                        color={"gris1"}
+                        fontSize={'1rem'}
+                        whiteSpace="nowrap"
+                        overflow="hidden"
+                        textOverflow="ellipsis"
+                        maxWidth="100%"
                         >
-                            <Image
-                            src={category?.imageUrl}
-                            h={"100%"}
-                            w={"100%"}
-                            objectFit={"cover"}
-                            ></Image>
-                        </CardBody>
-                        <CardFooter color={"negro"} alignContent={"center"} justify={"center"}>
-                            <Text
-                            fontFamily={"Saira"}
-                            color={"gris1"}
-                            fontSize={['1 rem', '1.2rem']}
-                            textShadow={"5px 5px 5px gray"}
-                            whiteSpace="nowrap"
-                            overflow="hidden"
-                            textOverflow="ellipsis"
-                            maxWidth="100%"
-                            >
-                            {category.categoryName}
-                            </Text>
-                        </CardFooter>
-                    </Card>
-            </ProductCardContainer>
-          ))}
-        </HStack>
-    //   )} 
-  );
-}
+                        {category.categoryName}
+                        </Checkbox>
+                    )
+                    )}                   
+                    </HStack>
+                    </>
+                )
+                 : 
+                (
+                    <HStack justify={"space-around"} w={"100%"} h={'100%'} my={5} display={"flex"} wrap={'wrap'}>
+                        {categories.map((category) => (
+                        <ProductCardContainer key={category.id} onClick={addCategory(category)}>
+                            <Card  
+                            _hover={{
+                                transform: "scale(1.02)", // Escala un poco la tarjeta en el hover
+                                //boxShadow: "md", // Agrega una sombra al hacer hover
+                                cursor: "pointer", // Cambia el cursor al pasar por encima
+                                }}
+                                border={"5px solid black"}
+                                h={['100px','150px',"180px"]}
+                                w={['100px','150px',"180px"]} 
+                                color={"blanco"}>
+                                <CardBody 
+                                    border={"1px solid black"}
+                                    display={"flex"}
+                                    justifyContent={"center"}
+                                    alignItems={'center'}
+                                    alignContent={'center'}
+                                    h={"99%"}
+                                    w={"99%"}
+                                    p={0}
+                                >
+                                    <Image
+                                    src={category?.imageUrl}
+                                    display={"flex"}
+                                    justifyContent={"center"}
+                                    alignItems={'center'}
+                                    alignContent={'center'}
+                                    h={"100%"}
+                                    w={"100%"}
+                                    objectFit={"cover"}
+                                    ></Image>
+                                </CardBody>
+                                <CardFooter color={"negro"} alignContent={"center"} justify={"center"}>
+                                    <Text
+                                    fontFamily={"Saira"}
+                                    color={"gris1"}
+                                    fontSize={'1 rem'}
+                                    whiteSpace="nowrap"
+                                    overflow="hidden"
+                                    textOverflow="ellipsis"
+                                    maxWidth="100%"
+                                    >
+                                    {category.categoryName}
+                                    </Text>
+                                </CardFooter>
+                            </Card>
+                        </ProductCardContainer>                    
+                        )
+                        )}           
+                    </HStack>
+                )
+          }
+        </VStack>
+      )
+    } 
+//   );
+
 
 export default FilterBar;
