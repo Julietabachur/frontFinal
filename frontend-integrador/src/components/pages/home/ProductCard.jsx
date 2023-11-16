897a14d9d63d0bc8fbd60a68ab903b8a39c5645c
TaniaAlicia <targ5181@gmail.com>
2023-11-15 22:47:40 -0300

    agregando corazon favoritos

--- frontend-integrador/src/components/pages/home/ProductCard.jsx
+++ frontend-integrador/src/components/pages/home/ProductCard.jsx
@@ -1,48 +1,73 @@
+import React, { useState } from "react";
 import {
   Box,
   Card,
-  CardHeader,
   CardBody,
   CardFooter,
-  HStack,
+  Button,
   Image,
   Text,
 } from "@chakra-ui/react";
+import { FaRegHeart } from "react-icons/fa";
 
 const ProductCard = ({ item }) => {
+  const [isHeartClicked, setHeartClicked] = useState(false);
+
+  const handleHeartClick = (event) => {
+    // Evitar que el evento se propague a la tarjeta
+    event.stopPropagation();
+    
+    // Cambiar el estado del clic del corazón
+    setHeartClicked(!isHeartClicked);
+  };
+
   return (
-    <Card  
-    h={500}
-    w={300} 
-    color={"blanco"}>
+    <Card
+      h={500}
+      w={300}
+      color={"blanco"}
       
+      
+    >
       <CardBody
         _hover={{
-          transform: "scale(0.98)", // Escala un poco la tarjeta en el hover
-          /* boxShadow: "inner",  */// Agrega una sombra al hacer hover
-          cursor: "pointer", // Cambia el cursor al pasar por encima
+          transform: "scale(0.98)",
+          cursor: "pointer",
         }}
         border={"5px solid black"}
-        boxShadow={'2xl'}
+        boxShadow={"2xl"}
         display={"flex"}
         justifyContent={"center"}
-        h={['90%']}
-        w={['100%']}
+        h={["90%"]}
+        w={["100%"]}
         p={0}
+        position="relative"
       >
+        {/* Imagen */}
         <Image
           src={item?.thumbnail}
           h={"100%"}
           w={"100%"}
           objectFit={"cover"}
-        ></Image>
+        />
+
+        {/* Botón de corazón personalizado */}
+        <Box position="absolute" top={1.5} right={1.5}>
+            <FaRegHeart 
+            size={30} 
+            color="green"
+            backgroundColor={isHeartClicked ? "green" : "transparent"}
+            onClick={handleHeartClick}
+            />
+          
+        </Box>
       </CardBody>
+
       <CardFooter color={"negro"} alignContent={"center"} justify={"center"}>
         <Text
           fontFamily={"Saira"}
           color={"gris1"}
-          fontSize={['1 rem', '1.2rem']}
-          // textShadow={"5px 5px 5px gray"}
+          fontSize={["1rem", "1.2rem"]}
           whiteSpace="nowrap"
           overflow="hidden"
           textOverflow="ellipsis"
