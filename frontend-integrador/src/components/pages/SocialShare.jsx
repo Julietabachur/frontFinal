c5e4163fd3ddc2f956a40c583b56a70d6bb6afda
PachoSur <pacho.baires@gmail.com>
2023-11-17 20:03:52 -0300

    Corregido Verify Alert y función compartir.

--- frontend-integrador/src/components/pages/SocialShare.jsx
+++ frontend-integrador/src/components/pages/SocialShare.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-  HStack,
-  VStack,
-  Image,
-  Text,
-  Box,
-  Button,
-  IconButton,
-  Stack,
-  Flex,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  } from "@chakra-ui/react";
-  import { FaFacebookSquare, FaInstagram, FaTwitter } from "react-icons/fa";
-  import { FcShare } from "react-icons/fc";
-  import DetailPage from "./DetailPage";
+    HStack,
+    VStack,
+    Image,
+    Text,
+    Box,
+    Button,
+    IconButton,
+    Stack,
+    Flex,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from "@chakra-ui/react";
+import { FaFacebookSquare, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FcShare } from "react-icons/fc";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from "react-share";
 
-  const SocialShare = ({openShareModal, setOpenShareModal, shareTitle, shareText, shareImage, shareUrl}) => {
+const SocialShare = ({ openShareModal, setOpenShareModal, shareTitle, shareText, shareImage, shareUrl }) => {
 
     console.log("Titulo: ", shareTitle);
     console.log("Texto: ", shareText);
@@ -31,67 +31,92 @@ import {
     console.log("Url: ", shareUrl);
 
 
-    const setShare = (origin) =>{
+    const setShare = (origin) => {
         if (origin == "facebook") {
-    
-          alert("PUBLICAR EN FACEBOOK");
-    
+
+            alert("PUBLICAR EN FACEBOOK");
+
+
+
+
+
         } else if (origin == "instagram") {
-    
-          alert("PUBLICAR EN Instagram");
-    
+
+            alert("PUBLICAR EN Instagram");
+
         } else if (origin == "twitter") {
-    
-          alert("PUBLICAR EN Twitter");
-    
+
+            alert("PUBLICAR EN Twitter");
+
         }
-    
-      };
+
+    };
 
     const handleCancel = () => {
         // Cierra el modal y resetea el formulario
         setOpenShareModal(false);
         //onClose();
-    
-      };
+
+    };
+
+    // Icono cuadrado con funcionalidad "onClick"
+    //<IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaInstagram />} onClick={() => setShare('instagram')} />
 
 
     return (
         <>
 
-        <Modal isOpen={openShareModal} onClose={handleCancel} size={'sm'}>
-        <ModalOverlay />
-        <ModalContent mt={130} maxWidth="40%">
-          <ModalHeader>Compartir</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody borderColor={"black"}>
-            <Flex  border={1}
-              flexDirection="column"
-              align={'center'}
-              p={1}
-              gap={5}
-              my={1}
-              maxHeight="45vh"
-            >
-              <Image w={120} h={150}  src={shareImage} alt={shareTitle} />
-              <Text size='sm'>{shareTitle}</Text>
-              <HStack>
+            <Modal isOpen={openShareModal} onClose={handleCancel} size={'sm'}>
+                <ModalOverlay />
+                <ModalContent mt={130} maxWidth="40%">
+                    <ModalHeader>Compartir</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody borderColor={"black"}>
+                        <Flex border={1}
+                            flexDirection="column"
+                            align={'center'}
+                            p={1}
+                            gap={5}
+                            my={1}
+                            maxHeight="45vh"
+                        >
+                            <Image w={120} h={150} src={shareImage} alt={shareTitle} />
+                            <Text size='sm'>{shareTitle}</Text>
+                            <Text size={'sm'}>{shareUrl}</Text>
+                            <HStack>
 
-              <IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaFacebookSquare />} onClick={() => setShare('facebook')}/>
-              <IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaInstagram />} onClick={() => setShare('instagram')}/>
-              <IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaTwitter />} onClick={() => setShare('twitter')}/>
-              </HStack>
+                                <div>
+                                    <FacebookShareButton
+                                        url={shareUrl}
+                                        quote={shareTitle}
+                                        hashtag="#riskko"
+                                    >
+                                        <IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaFacebookSquare />} />
 
-              </Flex>
-              </ModalBody>
-              </ModalContent>
-        </Modal>
+                                    </FacebookShareButton>
 
-        
-        
+                                </div>
+
+                                <TwitterShareButton
+                                    url={shareUrl}
+                                    quote={shareTitle}
+                                    hashtag="#riskko"
+                                >
+                                    <IconButton colorScheme='gray' variant='outline' size='lg' aria-label='Share' icon={<FaTwitter />} />
+                                </TwitterShareButton>
+
+                            </HStack>
+
+                        </Flex>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+
+
+
         </>
     );
 
-  };
+};
 
-  export default SocialShare;
+export default SocialShare;
