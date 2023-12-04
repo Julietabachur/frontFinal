4e131d5344fd6fd714f9ba8cd925306dcf9eded5
PachoSur <pacho.baires@gmail.com>
2023-12-04 00:07:58 -0300

    Nuevo Push con botón WA, y arreglos menores en SocialShare. visto Register y Login.

--- frontend-integrador/src/components/Footer.jsx
+++ frontend-integrador/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
-import { Box, Text, Image, VStack, Spacer, HStack  } from '@chakra-ui/react';
-import { FaFacebook,FaInstagram,FaWhatsapp } from 'react-icons/fa';
+import { Box, Text, Image, VStack, Spacer, HStack } from '@chakra-ui/react';
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
 
-  const whatsappLink = "https://wa.me/+573142523828";
+  const instagramLink = "https://instagram.com/";
+  const facebookLink = "https://facebook.com/";
 
   const [media, setMedia] = useState(false);
   const MIN_DESKTOP_WIDTH = 768;
@@ -34,42 +35,42 @@ const Footer = () => {
 
   return (
 
-          <>
-            {media ? (
-            <VStack bg={'#444444'} p={3} textAlign="center" w="99vw">
-              <HStack spacing='16px' py={'20px'}>
-              <Box w='40px' h='40px'>
-                <FaFacebook size={35} color={'#EDF2F7'}/>
-              </Box>
-              <Box w='40px' h='40px'>
-                <FaInstagram size={35} color={'#EDF2F7'}/>
-              </Box>
-              <Box w='40px' h='40px'>
-                <FaWhatsapp size={35} color={'#EDF2F7'}/>
-              </Box>
-            </HStack> 
-              <VStack
-              spacing={4}
-              align='stretch'
-              d='flex'
-              justifyContent="center"
-              alignItems="center"
-              my={3}
-            >
-                <Box h='40px' d='flex' justifyContent="center" alignItems="center">
-                  <Image ml={2} src="https://images-g3.s3.amazonaws.com/logoFooter.png" alt="Logo"/>
-                </Box>
-                <Box h='40px' d='flex' justifyContent="center" alignItems="center">
-                  <Text ml={2} fontSize="sm" color='gray.100' textAlign="left" as='b'>
-                    Copyright - 2023 - Digital House
-                  </Text>
-                </Box>
-              </VStack>
-            </VStack>            
-            )
-            :
-            (
-            <HStack bg={'#444444'} p={3} textAlign="center" w="99vw">
+    <>
+      {media ? (
+        <VStack bg={'#444444'} p={3} textAlign="center" w="99vw">
+          <HStack spacing='16px' py={'20px'}>
+            <Box w='40px' h='40px'>
+              <FaFacebook size={35} color={'#EDF2F7'} />
+            </Box>
+            <Box w='40px' h='40px'>
+              <FaInstagram size={35} color={'#EDF2F7'} />
+            </Box>
+            <Box w='40px' h='40px'>
+              <Text>HOLA</Text>
+            </Box>
+          </HStack>
+          <VStack
+            spacing={4}
+            align='stretch'
+            d='flex'
+            justifyContent="center"
+            alignItems="center"
+            my={3}
+          >
+            <Box h='40px' d='flex' justifyContent="center" alignItems="center">
+              <Image ml={2} src="https://images-g3.s3.amazonaws.com/logoFooter.png" alt="Logo" />
+            </Box>
+            <Box h='40px' d='flex' justifyContent="center" alignItems="center">
+              <Text ml={2} fontSize="sm" color='gray.100' textAlign="left" as='b'>
+                Copyright - 2023 - Digital House
+              </Text>
+            </Box>
+          </VStack>
+        </VStack>
+      )
+        :
+        (
+          <HStack bg={'#444444'} p={3} textAlign="center" w="99vw">
             <VStack
               spacing={4}
               align='stretch'
@@ -77,10 +78,10 @@ const Footer = () => {
               justifyContent="center"
               alignItems="start"
               my={5}
-             
+
             >
               <Box h='40px' d='flex' justifyContent="center" alignItems="start">
-                <Image ml={7} src="https://images-g3.s3.amazonaws.com/logoFooter.png" alt="Logo"/>
+                <Image ml={7} src="https://images-g3.s3.amazonaws.com/logoFooter.png" alt="Logo" />
               </Box>
               <Box h='40px' d='flex' justifyContent="center" alignItems="start">
                 <Text ml={7} fontSize="sm" color='gray.100' textAlign="left" as='b'>
@@ -88,27 +89,29 @@ const Footer = () => {
                 </Text>
               </Box>
             </VStack>
-            <Spacer />    
+            <Spacer />
             <HStack spacing='16px' pb={'50px'} mr={7}>
-              <Box w='40px' h='40px'>
-                <FaFacebook size={35} color={'#EDF2F7'}/>
-                
-              </Box>
-              <Box w='40px' h='40px'>
-                <FaInstagram size={35} color={'#EDF2F7'}/>
-              </Box>
-              <Box w='40px' h='40px'>
 
-              <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
-    <FaWhatsapp size={35} color={'#EDF2F7'} />
-  </a>
-              
+              <Box w='40px' h='40px'>
+                <a href={facebookLink} target="_blank" rel="noopener noreferrer">
+                  <FaFacebook size={35} color={'#EDF2F7'} />
+                </a>
               </Box>
-            </HStack> 
+
+              <Box w='40px' h='40px'>
+                <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={35} color={'#EDF2F7'} />
+                </a>
+              </Box>
+
+              <Box w='60px' h='40px'>
+              </Box>
+
             </HStack>
-            )
-            } 
-            </>
+          </HStack>
+        )
+      }
+    </>
   )
 }
 
