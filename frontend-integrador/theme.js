d38eeda5eb68b7e7f901045fa66a3e2243b54376
Julieta BACHUR <jbachur@CNP.com.ar>
2024-10-28 00:13:51 -0300

    renuevo navbar en desktop y mobile

--- frontend-integrador/theme.js
+++ frontend-integrador/theme.js
@@ -4,11 +4,7 @@ const theme = extendTheme({
   colors: {
     negro: "#000000",
     blanco: "#FFFFFF",
-    verde1: "#008800",
-    verde2:"#00cc00",
-    gris1:"#444444",
-    gris2:"#999999"
-
+    color: "#e1bc6a",
   },
 });
 
