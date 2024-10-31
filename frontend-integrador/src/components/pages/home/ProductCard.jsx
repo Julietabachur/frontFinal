f284a901d5b2785089c7c82e15272f7603ff038d
GodSavitar19 <106354483+GodSavitar19@users.noreply.github.com>
2024-10-30 23:07:29 -0300

    ProductCard Fix

--- frontend-integrador/src/components/pages/home/ProductCard.jsx
+++ frontend-integrador/src/components/pages/home/ProductCard.jsx
@@ -4,7 +4,6 @@ import {
   Card,
   CardBody,
   CardFooter,
-  Button,
   Image,
   Text,
   Link,
@@ -16,91 +15,71 @@ import { Link as ReactRouterLink } from "react-router-dom";
 const ProductCard = ({ item }) => {
   const [isHeartClicked, setHeartClicked] = useState(false);
   const { setFavorites, favorites } = useProductContext();
-
- 
   const token = JSON.parse(localStorage.getItem("riskkojwt"));
-
 
   const isFavorite = favorites.includes(item.id);
 
   useEffect(() => {
-    
-    const isFavorite = favorites.includes(item.id);
     setHeartClicked(isFavorite);
-  }, [item.id]);
+  }, [item.id, favorites]);
 
   const handleHeartClick = (event) => {
-    
+    event.stopPropagation(); // Evita la navegación al hacer clic en el corazón
     setHeartClicked(!isHeartClicked);
 
     const updatedFavorites = isHeartClicked
-      ? favorites.filter((id) => id !== item.id) 
-      : [...favorites, item.id]; 
+      ? favorites.filter((id) => id !== item.id)
+      : [...favorites, item.id];
 
     setFavorites(updatedFavorites);
   };
 
   return (
-    <Card  
-    h={[300,400,500]}
-    w={[198,264,330]} 
-    color={"blanco"}>
-      
-      <CardBody
-        _hover={{
-          transform: "scale(0.98)",
-          cursor: "pointer",
-        }}
-        border={"3px solid black"}
-        boxShadow={'2xl'}
-        display={"flex"}
-        justifyContent={"center"}
-        h={["90%"]}
-        w={["100%"]}
-        p={0}
-        position="relative"
+    <Link as={ReactRouterLink} to={`/detalle/${item.id}`} style={{ textDecoration: "none" }}>
+      <Card
+        h={[300, 400, 500]}
+        w={[198, 264, 330]}
+        color="blanco"
+        _hover={{ transform: "scale(0.98)", cursor: "pointer" }}
+        border="3px solid black"
+        boxShadow="2xl"
       >
-        {}
-        <Link as={ReactRouterLink} to={`/detalle/${item.id}`}>
+        <CardBody display="flex" justifyContent="center" h="90%" p={0} position="relative">
           <Image
             src={item?.thumbnail}
-            h={"100%"}
-            w={"100%"}
-            objectFit={"cover"}
+            h="100%"
+            w="100%"
+            objectFit="cover"
           />
-        </Link>
-        {}
-        {token && (
-          <Box
-            position="absolute"
-            top={1.5}
-            right={1.5}
-            onClick={handleHeartClick}
-            color="green"
-            _hover={{
-              color: "green",
-            }}
+          {token && (
+            <Box
+              position="absolute"
+              top={1.5}
+              right={1.5}
+              onClick={handleHeartClick}
+              color="green"
+              _hover={{ color: "darkgreen" }}
+            >
+              {isFavorite ? <FaHeart size={30} /> : <FaRegHeart size={30} />}
+            </Box>
+          )}
+        </CardBody>
+        <CardFooter color="negro" textAlign="center">
+          <Text
+            fontFamily="Roboto"
+            color="gris1"
+            fontWeight="semibold"
+            fontSize={["1rem", "1.2rem"]}
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+            maxWidth="100%"
           >
-            {isFavorite ? <FaHeart size={30} /> : <FaRegHeart size={30} />}
-          </Box>
-        )}
-      </CardBody>
-
-      <CardFooter color={"negro"} alignContent={"center"} justify={"center"}>
-        <Text
-          fontFamily={"Roboto"}
-          color={"gris1"}
-          fontWeight="semibold"
-          fontSize={["1rem", "1.2rem"]}
-          whiteSpace="nowrap"
-          overflow="hidden"
-          textOverflow="ellipsis"
-          maxWidth="100%"
-        >
-          {item.productName}
-        </Text>
-      </CardFooter>
-    </Card>
+            {item.productName}
+          </Text>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
