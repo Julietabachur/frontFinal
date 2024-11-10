d6c426bb0342c5f2c5572be54eefd4bb585b23a8
Julieta BACHUR <jbachur@CNP.com.ar>
2024-11-10 20:05:22 -0300

    Creo titulo al filtrar

--- frontend-integrador/src/components/pages/home/Global.context.jsx
+++ frontend-integrador/src/components/pages/home/Global.context.jsx
@@ -51,6 +51,8 @@ const reducer = (state, action) => {
       return { ...state, banderaReservas: action.payload };
     case "SET_IS_SIGN_IN":
       return { ...state, isSignIn: action.payload };
+    case "SET_TITULO":
+      return { ...state, titulo: action.payload };
     case "SET_IS_FILTERED_BY_CATEGORY":
       return { ...state, isFilteredByCategory: action.payload };
     default:
@@ -59,6 +61,7 @@ const reducer = (state, action) => {
 };
 // Estado inicial del contexto global
 const initialState = {
+  titulo:'',
   paginatedData: [],
   season: '',
   paginatedDataBySeason:[],
@@ -108,6 +111,9 @@ const ProductProvider = ({ children }) => {
 
   const setSearchResults = (data) => {
     dispatch({ type: "SET_SEARCH_RESULTS", payload: data });
+  };
+  const setTitulo = (data) => {
+    dispatch({ type: "SET_TITULO", payload: data });
   };
   const setIsSignIn = (data) => {
     dispatch({ type: "SET_IS_SIGN_IN", payload: data });
@@ -195,6 +201,8 @@ const ProductProvider = ({ children }) => {
         data.content.sort(() => Math.random() - 0.5);
         setPaginatedDataBySeason([])
         setPaginatedData(data);
+        setIsFilteredByCategory(true)
+        setTitulo('Todos nuestros productos')
       }
     } catch (error) {
       console.error(error);
@@ -214,6 +222,8 @@ const ProductProvider = ({ children }) => {
       if (response) {
 
         setPaginatedData(response.data);
+        setIsFilteredByCategory(true)
+        setTitulo(`Categoría: ${categories}`)
       }
     } catch (error) {
       console.error(error);
@@ -221,6 +231,8 @@ const ProductProvider = ({ children }) => {
   };
 
   const getProductsBySeason = async (page = 1) => {
+    debugger
+    setPaginatedData([])   
     try {
       const response = await axios.get(
         `${baseUrl}/api/v1/public/products/searchBySeason?season=${state.season}&page=${page}`,
@@ -232,8 +244,11 @@ const ProductProvider = ({ children }) => {
       );
       if (response) {
         console.log('Productos segun temporada', response.data);   
-        setPaginatedData([])     
-        setPaginatedDataBySeason(response.data);
+        setIsFilteredByCategory(false)          
+        setTimeout(() => {
+          setPaginatedDataBySeason(response.data); // setear productor por temp después de un pequeño retraso
+        }, 100); // 100ms de retraso        
+        setTitulo('')
       }
     } catch (error) {
       console.error(error);
@@ -261,11 +276,13 @@ const ProductProvider = ({ children }) => {
       );
 
       if (response.data && response.data.content) {
-      console.log(response);
       let data = response.data;
-      console.log(data);
       data.content.sort(() => Math.random() - 0.5);
       setPaginatedData(data);
+      setIsFilteredByCategory(true)
+      setTitulo(`Categorías seleccionadas: ${categories.join(", ")}`)
+      console.log(state.titulo);
+      
     } else {
       console.warn("No se encontraron productos para las categorías especificadas.");
     }
@@ -307,7 +324,7 @@ const ProductProvider = ({ children }) => {
 
   useEffect(() => {
     // Ejecutar getProductsBySeason si hay una temporada establecida
-    debugger
+    // debugger
     if (state.season) {
       getProductsBySeason();
     } else if (state.categories.length === 0) {
@@ -390,6 +407,8 @@ const ProductProvider = ({ children }) => {
     reservation: state.reservation,
     banderaReservas: state.banderaReservas,
     isSignIn:state.isSignIn, 
+    titulo: state.titulo,
+    setTitulo,
     setIsSignIn,
     setReservation,
     setShowFav,
