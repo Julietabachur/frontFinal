5b73f09efa79980d611697b0d596c0eb852e8f80
DiegoParula <parula27@gmail.com>
2024-11-27 20:41:13 -0300

    agregando loading despues de confirmar el pago

--- frontend-integrador/src/components/pages/cart/Succes.jsx
+++ frontend-integrador/src/components/pages/cart/Succes.jsx
@@ -1,23 +1,41 @@
 import { Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react' // Asegúrate de importar useState y useEffect
+
+import PaymentLoading from './PaymentLoading'
 
 function Succes() {
-  return (
-    <VStack
-        m={1}
-        w={"98vw"}
-        display={"flex"}
-        justifyContent={"start"}
-        px={20}
-        pt={4}
-        pb={10}
+  const [loading, setLoading] = useState(true); // Estado de loading
 
-      >
-        <Text mx={4} fontWeight="medium" fontFamily={"Roboto"} textAlign={'center'} fontSize={{base:'lg',md:"2xl"}} color={'#e1bc6a'} mt={'20px'}>
-                    GRACIAS POR TU COMPRA
-                </Text>
-      </VStack>
-  )
+ 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); 
+    }, 5000); 
+  }, []); 
+
+  return (
+    <>
+      {loading ? (
+        // Pantalla de carga (loading)
+        <PaymentLoading />
+      ) : (
+        // Contenido después de la carga
+        <VStack
+          m={1}
+          w={"98vw"}
+          display={"flex"}
+          justifyContent={"start"}
+          px={20}
+          pt={4}
+          pb={10}
+        >
+          <Text mx={4} fontWeight="medium" fontFamily={"Roboto"} textAlign={'center'} fontSize={{ base: 'lg', md: "2xl" }} color={'#e1bc6a'} mt={'20px'}>
+            GRACIAS POR TU COMPRA
+          </Text>
+        </VStack>
+      )}
+    </>
+  );
 }
 
-export default Succes
+export default Succes;
